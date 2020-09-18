6485131a914c338cf5c40c2150d2b3433eb349a6
Dalton Varney <daltonvarney@Daltons-Air.lan>
2020-09-17 17:34:38 -0700

    adding files

---
+++ README.md
@@ -0,0 +1,45 @@
+# Occupancy Detector
+This app expands on the *realtime_object_detector* starter app to build a occupancy app that takes a picture of each new person who enters the frame and keeps track of how many people are in the frame. The full tutorial can be found on the [alwaysAI blog](https://learn.alwaysai.co/detect-people-using-alwaysai).
+
+## Setup
+This app requires an alwaysAI account. Head to the [Sign up page](https://www.alwaysai.co/dashboard) if you don't have an account yet. Follow the instructions to install the alwaysAI toolchain on your development machine.
+
+Next, create an empty project to be used with this app. When you clone this repo, you can run `aai app configure` within the repo directory and your new project will appear in the list.
+
+## Usage
+Once you have the alwaysAI tools installed and the new project created, run the following CLI commands at the top level of the repo:
+
+To set the project, and select the target device run:
+
+```
+aai app configure
+```
+
+To build your app and deploy to the target device:
+
+```
+aai app install
+```
+
+To start the app:
+
+```
+aai app start
+```
+
+Images will be saved in the app directory with a person detection index and the timestamp. The timezone is set in the `Dockerfile` and may need to be changed based on your location.
+
+Once started, a link will appear for the Streamer which displays the camera feed and detections in your browser:
+
+```
+Loaded model:
+alwaysai/mobilenet_ssd
+
+Engine: Engine.DNN
+Accelerator: Accelerator.GPU
+
+Labels:
+['background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+
+[INFO] Streamer started at http://localhost:5000
+```
---
+++ app.py
@@ -0,0 +1,106 @@
+import time
+import edgeiq
+import cv2
+"""
+Use object detection to detect objects in the frame in realtime. The
+types of objects detected can be changed by selecting different models.
+
+To change the computer vision model, follow this guide:
+https://dashboard.alwaysai.co/docs/application_development/changing_the_model.html
+
+To change the engine and accelerator, follow this guide:
+https://dashboard.alwaysai.co/docs/application_development/changing_the_engine_and_accelerator.html
+"""
+
+
+def save_snapshot(image, person_id):
+    snap_date = time.strftime('%Y.%m.%d')
+    snap_time = time.strftime('%H.%M.%S')
+    filename = '{}-{}-person-{}.jpg'.format(snap_date, snap_time, person_id)
+    cv2.imwrite(filename, image)
+
+
+def main():
+    obj_detect = edgeiq.ObjectDetection(
+            "alwaysai/mobilenet_ssd")
+    obj_detect.load(engine=edgeiq.Engine.DNN)
+    tracker = edgeiq.CentroidTracker(deregister_frames=30)
+
+    print("Loaded model:\n{}\n".format(obj_detect.model_id))
+    print("Engine: {}".format(obj_detect.engine))
+    print("Accelerator: {}\n".format(obj_detect.accelerator))
+    print("Labels:\n{}\n".format(obj_detect.labels))
+
+    fps = edgeiq.FPS()
+
+    try:
+        with edgeiq.WebcamVideoStream(cam=0) as video_stream, \
+                edgeiq.Streamer() as streamer:
+            # Allow Webcam to warm up
+            time.sleep(2.0)
+            fps.start()
+
+            prev_tracked_people = {}
+            logs = []
+            currentPeople = 0
+
+            # loop detection
+            while True:
+                frame = video_stream.read()
+                results = obj_detect.detect_objects(frame, confidence_level=.5)
+                people = edgeiq.filter_predictions_by_label(results.predictions, ['person'])
+                tracked_people = tracker.update(people)
+
+                people = []
+                for (object_id, prediction) in tracked_people.items():
+                    new_label = 'Person {}'.format(object_id)
+                    prediction.label = new_label
+                    people.append(prediction)
+
+                frame = edgeiq.markup_image(
+                        frame, people, colors=obj_detect.colors)
+
+                new_entries = set(tracked_people) - set(prev_tracked_people)
+                for entry in new_entries:
+                    save_snapshot(frame, entry)
+                    logs.append('Person {} entered'.format(entry))
+                    currentPeople += 1
+
+                new_exits = set(prev_tracked_people) - set(tracked_people)
+                for exit in new_exits:
+                    logs.append('Person {} exited'.format(exit))
+                    currentPeople -= 1
+
+                prev_tracked_people = dict(tracked_people)
+
+                # Generate text to display on streamer
+                text = ["Model: {}".format(obj_detect.model_id)]
+                text.append(
+                        "Inference time: {:1.3f} s".format(results.duration))
+                text.append("Objects:")
+
+                for prediction in people:
+                    text.append("{}: {:2.2f}%".format(
+                        prediction.label, prediction.confidence * 100))
+
+                
+                text.append('Current Occupancy:')
+                text += str(currentPeople)
+
+                streamer.send_data(frame, text)
+
+                fps.update()
+
+                if streamer.check_exit():
+                    break
+
+    finally:
+        fps.stop()
+        print("elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
+        print("approx. FPS: {:.2f}".format(fps.compute_fps()))
+
+        print("Program Ending")
+
+
+if __name__ == "__main__":
+    main()
